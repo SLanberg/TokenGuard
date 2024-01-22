57bfad41363c6192266161fed7241c09207ab5a6
Stephan Lanberg <stefan.lnaberg@gmail.com>
2024-01-22 22:36:38 +0200

    Add initial routing

--- src/routes/sign-up/+page.svelte
+++ src/routes/sign-up/+page.svelte
@@ -5,21 +5,23 @@
     let thisForm:HTMLFormElement;
 </script>
 
-
 <h2>IMAGE IS HERE</h2>
 <form bind:this={thisForm} use:enhance method="post" action="?/signup">
     <div class="form-item">
 
         <label for="TelegramID">
-            TelegramID
+            Firstname<sup><small>*</small></sup>
             <input class:fieldError={form?.telegramIdUsed} value={form?.telegramID ?? ""} id="telegramID" type="text" name="telegramID" required />
         </label>
 
         <label for="firstName">
-            Password
+            Firstname<sup><small>*</small></sup>
             <input class:fieldError={form?.weakPassword, form?.passNotMatch} value={form?.password ?? ""} id="password" type="password" name="password" required />
         </label>
 
-        <p><u><a href="/sign-up">Sign Up</a></u></p>
+        <label for="firstName">
+            Firstname<sup><small>*</small></sup>
+            <input value={form?.confirmPassword ?? ""} id="confirmPassword" type="password" name="confirmPassword" required />
+        </label>
     </div>
 </form>
