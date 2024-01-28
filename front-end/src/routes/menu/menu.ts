12dbebb9ade124a3aa152b69f26d0706a27a8115
SLanberg <stefan.lanberg@gmail.com>
2024-01-28 20:50:07 +0200

    Add small fix

--- front-end/src/routes/menu/menu.ts
+++ front-end/src/routes/menu/menu.ts
@@ -1,5 +1,14 @@
 import { goto } from '$app/navigation';
+import {handleLoadEventsProfilePage} from "../../state/menuState";
 
 export const handleAvatarClick = async () => {
+    handleLoadEventsProfilePage.update(() => ({
+        loadProfilePage: true
+    }));
+
     await goto('/secure-access', {});
+
+    handleLoadEventsProfilePage.update(() => ({
+        loadProfilePage: false
+    }));
 }
