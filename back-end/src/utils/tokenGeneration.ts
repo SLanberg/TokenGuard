3d2e4f43ae5002250a41357a0e79fd57d87cbc04
SLanberg <stefan.lanberg@gmail.com>
2024-02-19 22:09:03 +0200

    Apply hash to generated token

--- back-end/src/utils/tokenGeneration.ts
+++ back-end/src/utils/tokenGeneration.ts
@@ -1,17 +1,14 @@
+import { BinaryLike, createHash } from 'crypto';
+
+function hash(string: BinaryLike) {
+  return createHash('sha256').update(string).digest('hex');
+}
+
 export const generateToken = (telegramID: number) => {
   const randomPart1 = Math.random().toString(36).slice(2, 6);
   const randomPart2 = Math.random().toString(36).slice(2, 6);
   const randomPart3 = Math.random().toString(36).slice(2, 6);
-  const randomPart4 = Math.random().toString(36).slice(2, 6);
 
-  // Here is used telegramID to ensure uniqueness of the generated token,
-  // To avoid collisions. It can of course be done with other perfect hash functions alternative
-  // But since this is a race with time and time is very short we will do it like that.
-  //
-  // The best approach is to take telegramID -> generate unique hash based on it
-  // and do the same for the part 1, 2, 3, 4
-  // of course it is important to implement also such
-  // thing as user enters incorrect token 3 times -> we freeze or block account to prevent Brute Force
-  // currently such security measures is not implemented
-  return `${telegramID}-${randomPart1}-${randomPart2}-${randomPart3}-${randomPart4}`;
+  const not_hashed_token = `${telegramID}-${randomPart1}-${randomPart2}-${randomPart3}`
+  return hash(not_hashed_token);
 }
